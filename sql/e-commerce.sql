e1aa88237eb7ef0946c29784eb8c254ff58e9167
elwazatikhadija <127736014+elwazatikhadija@users.noreply.github.com>
2024-01-21 18:52:13 +0100

    Premier commit

---
+++ sql/e-commerce.sql
@@ -0,0 +1,11 @@
+
+--drop table  if exists items ;
+--drop table  if exists item_seq;
+--drop table  if exists orders;
+--drop table  if exists order_seq;
+--drop table  if exists customer_seq;
+--drop table  if exists customer_orders_seq;
+--drop table  if exists customer_orders;
+--drop table  if exists customer;
+
+
